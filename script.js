ffd67ca5831414c0c1c6234cc39904b0b3e6e209
Miguel-2406 <miguel.al.camargo@gmail.com>
2026-06-01 00:07:30 -0300

    maior valor

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ function mensagemBoasVindas(nome) {
   return `Olá, ${nome}!`;
 }
 
-module.exports = { somar, mensagemBoasVindas };
+const maiorValor = (a, b) => {
+  if (a > b) return `${a} é o maior valor`;
+  if (b > a) return `${b} é o maior valor`;
+  return "Os valores são iguais";
+};
+
+module.exports = { somar, mensagemBoasVindas, maiorValor };
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const { somar, mensagemBoasVindas } = require('./script');
+const { somar, mensagemBoasVindas, maiorValor } = require('./script');
 
 test('soma dois números', () => {
   expect(somar(2, 3)).toBe(5);
@@ -6,4 +6,10 @@ test('soma dois números', () => {
 
 test('mensagem de boas vindas', () => {
   expect(mensagemBoasVindas('Diogo')).toBe('Olá, Diogo!');
+});
+
+describe("maiorValor", () => {
+  test("primeiro maior", () => expect(maiorValor(10, 5)).toBe("10 é o maior valor"));
+  test("segundo maior", () => expect(maiorValor(3, 99)).toBe("99 é o maior valor"));
+  test("iguais", () => expect(maiorValor(7, 7)).toBe("Os valores são iguais"));
 });
